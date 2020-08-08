38581cb40c4b864259c2fb73943b39154e144ab4
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2020-08-08 14:26:36 +0200

    feat: add models definition

--- models.py
+++ models.py
@@ -55,6 +55,48 @@ class Net(nn.Module):
         return x
 
 
+class NetComplex(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        # 1st structure (final feature map size = (224+2*0-5)/1 + 1 = 220, after poolling (220-2)/2 + 1 = 110)
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, stride=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 2nd structure (final feature map size = (110+2*0-3)/1 + 1 = 108, after poolling (108-2)/2 + 1 = 54)
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 3rd structure (final feature map size = (54+2*0-3)/1 + 1 = 52, after poolling (52-2)/2 + 1 = 26)
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, stride=1
+        )
+        self.bn3 = nn.BatchNorm2d(128)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # 4th structure (size input of flattened feature map = 128*26*26)
+        self.fc1 = nn.Linear(in_features=128 * 26 * 26, out_features=2048)
+        self.bn4 = nn.BatchNorm1d(2048)
+        self.dropout1 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=2048, out_features=512)
+        self.bn5 = nn.BatchNorm1d(512)
+        self.dropout2 = nn.Dropout(p=0.25)
+        self.fc3 = nn.Linear(in_features=512, out_features=136)
+
+    def forward(self, x):
+        x = self.maxpool1(F.relu(self.bn1(self.conv1(x))))
+        x = self.maxpool2(F.relu(self.bn2(self.conv2(x))))
+        x = self.maxpool3(F.relu(self.bn3(self.conv3(x))))
+        x = torch.flatten(x, 1)
+        x = self.dropout1(F.relu(self.bn4(self.fc1(x))))
+        x = self.dropout2(F.relu(self.bn5(self.fc2(x))))
+        x = self.fc3(x)
+
+        return x
+
+
 class Resnet18_gray(nn.Module):
     def __init__(self):
         super().__init__()
@@ -68,5 +110,32 @@ class Resnet18_gray(nn.Module):
         self.resnet18.fc = nn.Linear(fc_inputs, 2 * 68)
 
     def forward(self, x):
-
         return self.resnet18(x)
+
+
+class Squeezenet10_gray(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.squeezenet = models.squeezenet1_0(pretrained=True)
+        self.squeezenet.features[0] = nn.Conv2d(1, 96, kernel_size=7, stride=2)
+        self.squeezenet.classifier[1] = nn.Conv2d(
+            512, 2 * 68, kernel_size=(1, 1), stride=(1, 1)
+        )
+
+    def forward(self, x):
+        return self.squeezenet(x)
+
+
+class Mobilenet2_gray(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.mobilenet = models.mobilenet_v2(pretrained=True)
+        self.mobilenet.features[0][0] = nn.Conv2d(
+            1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False
+        )
+        self.mobilenet.classifier[1] = nn.Linear(
+            in_features=1280, out_features=2 * 68, bias=True
+        )
+
+    def forward(self, x):
+        return self.mobilenet(x)
