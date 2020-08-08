d32d9743f0972a263f5457755570d12e2e08259b
Łukasz Woronkiewicz <woronkiewicz@gmail.com>
2020-08-08 14:38:48 +0200

    feat: detect keypoints with laptop camera

--- detect_from_camera.py
+++ detect_from_camera.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
 import torch
-from models import Net
+from models import Resnet18_gray
 
 
-def detect_keypoints(save=False):
+def detect_keypoints(img_size, save=False):
 
     # capture image from camera
     cap = cv2.VideoCapture(-1)
@@ -18,8 +18,10 @@ def detect_keypoints(save=False):
         "detector_architectures/haarcascade_frontalface_default.xml"
     )
     # keypoint prediction
-    model = Net()
-    state_dict = torch.load("saved_models/keypoints_model_adam_001_20ep_amsgrad.pt")
+    model = Resnet18_gray()
+    state_dict = torch.load(
+        "saved_models/keypoints_resnet18_final.pt", map_location=torch.device("cpu"),
+    )
     model.load_state_dict(state_dict)
     model.eval()
 
@@ -44,39 +46,37 @@ def detect_keypoints(save=False):
         # change to gray and perform face detection
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(
-            gray, scaleFactor=1.15, minNeighbors=3, minSize=(75, 75)  # 1.2, 2
+            gray, scaleFactor=1.15, minNeighbors=3, minSize=(75, 75),
         )
 
         # display the resulting frame
         for (x, y, w, h) in faces:
-            # draw a rectangle around each detected face
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
-
             # select the region of interest that is the face in the image
-            d = 50
-            y_minus = y if y < d else d
-            y_plus = height - h - y if y + h + d > height else d
-            x_minus = x if x < d else d
-            x_plus = width - w - x if x + h + d > width else d
-            roi = gray[y - y_minus : y + h + y_plus, x - x_minus : x + w + x_plus]
+            roi = gray[y : y + h, x : x + w]
 
             ## normalize, rescale and reshape
-            roi_normalized = roi / 255.0
             shape_before_resize = roi.shape
-            roi_rescaled = cv2.resize(roi_normalized, (224, 224))
-            shape_after_resize = roi_rescaled.shape
+            roi_rescaled = cv2.resize(roi, (img_size, img_size))
+            roi_normalized = (
+                roi_rescaled / 255.0 / 255.0
+            )  # due to mpimg.imread(image_name) where reading training data
+            shape_after_resize = roi_normalized.shape
             scaling_factor_y = shape_before_resize[0] / shape_after_resize[0]
             scaling_factor_x = shape_before_resize[1] / shape_after_resize[1]
-            roi_torch = torch.from_numpy(roi_rescaled.reshape((1, 1, 224, 224))).float()
+            roi_torch = torch.from_numpy(
+                roi_normalized.reshape((1, 1, img_size, img_size))
+            ).float()
 
             ## predict
             pred_keypoints = model(roi_torch)
             keypoints_np = pred_keypoints.view((68, -1)).data.numpy()
-            keypoints_unnorm = keypoints_np * 50 + 100
+            keypoints_unnorm = keypoints_np * (roi_normalized.shape[0] / 4) + (
+                roi_normalized.shape[0] / 2
+            )
             keypoints_list = [
                 (
-                    int(np.round(ptx * scaling_factor_x, 0) + x - x_minus),
-                    int(np.round(pty * scaling_factor_y, 0) + y - y_minus),
+                    int(np.round(ptx * scaling_factor_x, 0) + x),  # - x_minus),
+                    int(np.round(pty * scaling_factor_y, 0) + y),  # - y_minus),
                 )
                 for ptx, pty in zip(keypoints_unnorm[:, 0], keypoints_unnorm[:, 1])
             ]
@@ -84,18 +84,29 @@ def detect_keypoints(save=False):
             # add keypoints to frame
             for i in range(len(keypoints_list)):
 
-                if i in [16, 21, 26, 35, 41, 47, 59, 67]:
+                if i in [16, 21, 26, 35]:
                     continue
 
                 prev = keypoints_list[i]
-                next = keypoints_list[i + 1]
+                if i not in [30, 41, 47, 59, 67]:
+                    next = keypoints_list[i + 1]
+                elif i == 30:
+                    next = keypoints_list[33]
+                elif i == 41:
+                    next = keypoints_list[36]
+                elif i == 47:
+                    next = keypoints_list[42]
+                elif i == 59:
+                    next = keypoints_list[48]
+                elif i == 67:
+                    next = keypoints_list[60]
 
                 # colors
                 if i < 16 or (i > 25 and i < 35):
                     color = (85, 222, 250)  # contour and nose
                 elif i < 26:
-                    color = (33, 67, 79)  # eyebrows
-                elif i < 47:
+                    color = (255, 89, 252)  # eyebrows
+                elif i < 48:
                     color = (245, 133, 34)  # eyes
                 else:
                     color = (69, 69, 247)  # mouth
@@ -121,4 +132,4 @@ def detect_keypoints(save=False):
 
 
 if __name__ == "__main__":
-    detect_keypoints(save=True)
+    detect_keypoints(img_size=224, save=True)
